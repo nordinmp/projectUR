43f2e571c1ae67029f8c447e634e1b73f4d5c038
NordinP22R <49874265+nordinmp@users.noreply.github.com>
2023-11-10 14:24:34 +0100

    Virker ikke helt
    Fordi millis tæller lige så snart programmet starter og ikke når vi trykker på knap

--- projectUR/functions.h
+++ projectUR/functions.h
@@ -1,37 +1,60 @@
 void GaetTid ()
 {
-int buttonPressedCount = 1;
-    
-    lcd.setRGB(colorR, colorG, colorB);
+  int buttonPressedCount = 1;
+  int buttonState = 0; 
+  const int buttonPin = 4;
+  int timeGuess = random(1000,10000);
 
-    lcd.print("Gæt tid");
+  unsigned long startTime = 0;
 
-    buttonState = digitalRead(buttonPin);
-    Serial.print(random(1000,10000));
-    int count = 0;
-    int yourTime = 0;
+  int count = 0;
+  int yourTime = 0;
+  int result = 0;
 
-        // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH && ButtonPressedCount == 1) 
+  pinMode(buttonPin, INPUT);
+
+  while(true) // We start a loop here to continuously check the button state
   {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    buttonPressedCount = buttonPressedCount + 1;
-    count = millis();
+    buttonState = digitalRead(buttonPin); // Update button state
 
-  }
-  else if(buttonState == HIGH && ButtonPressedCount == 2)
-  {
-    buttonPressedCount = buttonPressedCount + 1;
-    yourTime = count;
+    if (buttonState == HIGH && buttonPressedCount == 1) 
+    {
+      Serial.print("Tryk når du tror tiden er gået");
+      Serial.print("Tiden du skal gætte er ");
+      Serial.print(timeGuess);
+      delay(50);
+      buttonPressedCount++;
+      buttonState = 0;
+    }
+    else if(buttonState == HIGH && buttonPressedCount == 2)
+    {
+      startTime = millis(); // Store the start time
+      count = millis() - startTime; // Calculate the elapsed time
+      yourTime = count;
+      Serial.print("Tæller...");
+      buttonPressedCount++;
+      buttonState = 0;
+    }
+    else if(buttonState == HIGH && buttonPressedCount == 3)
+    {
+      Serial.print("Tryk igen for at se resultater");
+      delay(50);
+      buttonPressedCount++;
+      buttonState = 0;
+    }
+    else if(buttonState == HIGH && buttonPressedCount == 4)
+    {
+      yourTime = count;
+      Serial.print("Your time is: ");
+      Serial.println(yourTime); // This line will print the value of yourTime
+      result = timeGuess - yourTime;
+      Serial.print("Result: ");
+      Serial.println(result); // This line will print the result
+      delay(50);
+      buttonPressedCount = 1;
+      buttonState = 0;
+    }
 
-  }
-
-  else if(buttonState == HIGH && ButtonPressedCount == 3)
-  {
-    yourTime = count;
-    Serial.print(yourTime);
-    result = count - yourTime;
-    Serial.print(result);
+    delay(100); // Debounce delay
   }
 }
--- projectUR/projectUR.ino
+++ projectUR/projectUR.ino
@@ -1,26 +1,28 @@
-#incude "functions.h"
+#include "functions.h"
 #include <Wire.h>
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
 
-const int buttonPin = 2;
-int buttonState = 0;
 
 void setup() 
 {
   // put your setup code here, to run once:
       // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
-    
-    delay(1000);
+  lcd.begin(16, 2);
+  
+  delay(1000);
+  Serial.begin(9600);
 
-      pinMode(buttonPin, INPUT);
+  const int buttonPin = 4;
+
+  pinMode(buttonPin, INPUT);
+
 
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-
+  GaetTid();
 }
